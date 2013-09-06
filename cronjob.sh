d22963d204e1ffa9fae3ff1e2d19c3775ada8cb5
Nils Werner <nils.werner@gmail.com>
2013-09-06 14:11:12 +0200

    Hostname als JSONP

--- cronjob.sh
+++ cronjob.sh
@@ -2,6 +2,6 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-echo -n "{\"hostname\":\"" > ${DIR}/json/hostname.json
+echo -n "hostnamecallback({\"hostname\":\"" > ${DIR}/json/hostname.json
 ip addr show wlan0 | grep inet | grep -v inet6 | awk -n '{print $2;}' | tr -d '\n' >> ${DIR}/json/hostname.json
-echo "\"}" >> ${DIR}/json/hostname.json
+echo "\"});" >> ${DIR}/json/hostname.json
