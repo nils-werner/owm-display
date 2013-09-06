ca59e2ae0aafba0252040ce37e493ec254451905
Nils Werner <nils.werner@gmail.com>
2013-09-06 15:28:49 +0200

    ip ist in /sbin/, das ist nicht im PATH bei cronjobs

--- cronjob.sh
+++ cronjob.sh
@@ -2,6 +2,7 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+# HOSTNAME
 echo -n "hostnamecallback({\"hostname\":\"" > ${DIR}/json/hostname.json
-ip addr show wlan0 | grep inet | grep -v inet6 | awk -n '{print $2;}' | tr -d '\n' >> ${DIR}/json/hostname.json
+/sbin/ip addr show wlan0 | grep inet | grep -v inet6 | awk -n '{print $2;}' | tr -d '\n' >> ${DIR}/json/hostname.json
 echo "\"});" >> ${DIR}/json/hostname.json
