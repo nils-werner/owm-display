65420777c28fa33c0d63f4c6e020a5911535d791
Nils Werner <nils.werner@gmail.com>
2013-09-06 12:41:20 +0200

    Geraet benutzt wlan0

--- cronjob.sh
+++ cronjob.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 echo -n "{\"hostname\":\"" > ${DIR}/json/hostname.json
-ip addr show eth0 | grep inet | grep -v inet6 | awk -n '{print $2;}' | tr -d '\n' >> ${DIR}/json/hostname.json
+ip addr show wlan0 | grep inet | grep -v inet6 | awk -n '{print $2;}' | tr -d '\n' >> ${DIR}/json/hostname.json
 echo "\"}" >> ${DIR}/json/hostname.json
